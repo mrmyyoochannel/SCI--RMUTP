0801abb6ce910d215eed3d926cc31b1ce07b4c65
MYYOOMI <93776430+mrmyyoochannel@users.noreply.github.com>
2025-02-28 11:29:18 +0700

    add new file and fix some issues
    add new file and fix some issues

---
+++ _php_body/Board.php
@@ -0,0 +1,45 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center">คณะบริหาร สโมสรนักศึกษา</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/board/board1.jpg" class="card-img-top" alt="Board 1">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อ-นามสกุล</h5>
+                    <p class="card-text">ตำแหน่ง</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/board/board2.jpg" class="card-img-top" alt="Board 2">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อ-นามสกุล</h5>
+                    <p class="card-text">ตำแหน่ง</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/board/board3.jpg" class="card-img-top" alt="Board 3">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อ-นามสกุล</h5>
+                    <p class="card-text">ตำแหน่ง</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/board/board4.jpg" class="card-img-top" alt="Board 4">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อ-นามสกุล</h5>
+                    <p class="card-text">ตำแหน่ง</p>
+                </div>
+            </div>
+        </div>
---
+++ _php_body/achievements.php
@@ -0,0 +1,76 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1 class="text-center">ผลงานและรางวัล</h1>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/achievements/achievement1.jpg" class="card-img-top" alt="Achievement 1">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อผลงาน/รางวัล</h5>
+                    <p class="card-text">รายละเอียด</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/achievements/achievement2.jpg" class="card-img-top" alt="Achievement 2">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อผลงาน/รางวัล</h5>
+                    <p class="card-text">รายละเอียด</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/achievements/achievement3.jpg" class="card-img-top" alt="Achievement 3">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อผลงาน/รางวัล</h5>
+                    <p class="card-text">รายละเอียด</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/achievements/achievement4.jpg" class="card-img-top" alt="Achievement 4">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อผลงาน/รางวัล</h5>
+                    <p class="card-text">รายละเอียด</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/board/board5.jpg" class="card-img-top" alt="Board 5">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อ-นามสกุล</h5>
+                    <p class="card-text">ตำแหน่ง</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/board/board6.jpg" class="card-img-top" alt="Board 6">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อ-นามสกุล</h5>
+                    <p class="card-text">ตำแหน่ง</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-4">
+            <div class="card">
+                <img src="images/board/board7.jpg" class="card-img-top" alt="Board 7">
+                <div class="card-body">
+                    <h5 class="card-title">ชื่อ-นามสกุล</h5>
+                    <p class="card-text">ตำแหน่ง</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
